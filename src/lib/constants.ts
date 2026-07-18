ea4f1458d4346efd2496faaaa0218a1fae48ab51
BJMack <bjmack@BJMacks-MacBook-Air.local>
2026-07-18 18:08:41 +0800

    Configure default super admin UID fallback

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,5 +3,6 @@ export const SUPER_ADMIN_EMAILS = [
     ...(process.env.SUPER_ADMIN_EMAILS || '').split(',').filter(Boolean)
 ];
 export const SUPER_ADMIN_UIDS = [
+    'bYkQRbFisqd9WcePA5WZmhpQPcB3',
     ...(process.env.SUPER_ADMIN_UIDS || '').split(',').filter(Boolean)
 ];
