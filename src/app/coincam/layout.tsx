5cbc77a3468223ac766abe43ca02baa8707760d0
BJMack <bjmack@BJMacks-MacBook-Air.local>
2026-07-25 20:25:12 +0800

    chore: clean character format in layout.tsx

--- src/app/coincam/layout.tsx
+++ src/app/coincam/layout.tsx
@@ -1,6 +1,6 @@
-import type {Metadata} from 'next';
+import type { Metadata } from 'next';
 import './globals.css';
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from '@/components/ui/toaster';
 import { AuthProvider } from './auth-provider';
 
 export const metadata: Metadata = {
